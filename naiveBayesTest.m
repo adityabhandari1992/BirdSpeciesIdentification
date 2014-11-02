b130e32706c5c648019dadc6487d40892027c68f
Aditya Bhandari <adityabhandari1992@gmail.com>
2014-11-02 14:44:13 -0800

    Added the phiY terms in train and test

--- naiveBayesTest.m
+++ naiveBayesTest.m
@@ -1,4 +1,4 @@
-function [test_image_labels, error] = naiveBayesTest(phi_attributeK_classI, image_class_labels, image_attribute_labels, numberAttributes, numberClasses)
+function [test_image_labels, error] = naiveBayesTest(phi_attributeK_classI, phiY, image_class_labels, image_attribute_labels, numberAttributes, numberClasses)
 
 % Find the number of images
 numberImages = size(image_class_labels,1);
@@ -8,25 +8,28 @@ test_image_labels = zeros(numberImages, 1);
 
 % Find the best class for each image based on the phi values
 for j = 1:numberImages
-    probability_classI = ones(numberClasses, 1);
+    probability_classI = zeros(numberClasses, 1);
     
-    % Find the probability of each class and find the maximum among them
+    % Find the logarithmic probability of each class and find the maximum among them
     maxProbability = 0;
     maxProbabilityClass = 0;
     for i = 1:numberClasses
         % Iterate over all the attributes
-        % Probability(i) = (Product over all k) phi(k,i)
+        % Probability(i) += (Sum over all k) log(phi(k,i))
         for k = 1:numberAttributes
             row = j.*k;
             if (image_attribute_labels(row,3) == 1)
-                probability_classI(i) = probability_classI(i).*phi_attributeK_classI(k,i);
+                probability_classI(i) = probability_classI(i) + log(phi_attributeK_classI(k,i));
             else
-                probability_classI(i) = probability_classI(i).*(1-phi_attributeK_classI(k,i));
+                probability_classI(i) = probability_classI(i) + log(1-phi_attributeK_classI(k,i));
             end
         end
         
+        % Add the log(P(y=i)) value
+        probability_classI(i) = probability_classI(i) + log(phiY(i));
+        
         % Update the maximum value if needed
-        if (probability_classI(i) > maxProbability)
+        if (maxProbabilityClass == 0 || probability_classI(i) > maxProbability)
             maxProbabilityClass = i;
             maxProbability = probability_classI(i);
         end
--- naiveBayesTrain.m
+++ naiveBayesTrain.m
@@ -1,4 +1,4 @@
-function [phi_attributeK_classI] = naiveBayesTrain(image_class_labels, image_attribute_labels, numberAttributes, numberClasses)
+function [phi_attributeK_classI, phiY] = naiveBayesTrain(image_class_labels, image_attribute_labels, numberAttributes, numberClasses)
 
 % Find the number of images
 numberImages = size(image_class_labels,1);
@@ -6,27 +6,30 @@ numberImages = size(image_class_labels,1);
 % Initialize the output matrix
 phi_attributeK_classI = zeros(numberAttributes,numberClasses);
 
-% We need to find the phi values for each attribute given each class
+% We find the P(y=i) for each class 'i'
+phiY = zeros(numberClasses, 1);
+
+% We find the phi values for each attribute given each class
 for k = 1:numberAttributes
     % phi_attributeK_classI(k,i) = numeratorPhiKI(i) / denominatorPhiKI(i)
     % Initialize numerators and denominators for Laplace smoothing
-    numeratorPhiKI = ones(1, numberClasses);
-    denominatorPhiKI = numberAttributes * ones(1, numberClasses);
+    numeratorPhiKI = ones(numberClasses, 1);
+    denominatorPhiKI = numberAttributes * ones(numberClasses, 1);
     
     % Iterate over the data where attribute is k
     for j = 1:numberImages
         row = j.*k;
         % If the image belongs to class 'i'
-        % Update numeratorPhiKI(i) and denominatorPhiKI(i)
+        % Update phiY(i), numeratorPhiKI(i) and denominatorPhiKI(i)
         i = image_class_labels(j,2);
-        if (image_attribute_labels(row,3) == 1)
-            numeratorPhiKI(i) = numeratorPhiKI(i) + 1;
-        end
+        phiY(i) = phiY(i) + 1;
+        numeratorPhiKI(i) = numeratorPhiKI(i) + image_attribute_labels(row,3);
         denominatorPhiKI(i) = denominatorPhiKI(i) + 1;
     end
     
-    % Store the phi value for attribute k given class 'i'
+    % Store the phi value for y and for attribute k given class 'i'
     for i = 1:numberClasses
+        phiY(i) = phiY(i)./numberImages;
         phi_attributeK_classI(k,i) = numeratorPhiKI(i)./denominatorPhiKI(i);
     end
 end
