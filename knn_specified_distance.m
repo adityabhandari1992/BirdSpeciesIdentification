efb90d9193c19329e9ab5473407266b841e445d5
ameya <ameya.joshi@iitgn.ac.in>
2014-11-03 10:42:14 -0800

    Added a choice of distance measures

---
+++ knn_specified_distance.m
@@ -0,0 +1,26 @@
+function [B, optimum_K accuracy]=knn_specified_distance(validation_matrix,validation_matrix_labels,training_matrix,training_matrix_classes,K_training_range_row,distance)
+%knn_mat=validation_matrix*training_matrix';
+%distance='euclidean';
+% 'hamming' , 'chebychev' , 'minkowski', 'euclidean', 'seuclidean'
+knn_mat = pdist2(validation_matrix,training_matrix,distance);
+score_size=size((training_matrix),1);
+score=zeros([score_size 1]);
+validation_matrix_size=size((validation_matrix),1);
+K_training_range_row_size=size((K_training_range_row),2);
+for k=1:K_training_range_row_size
+
+    for v=1:validation_matrix_size
+    [B, I]=sort(knn_mat(v,:));
+    class_residual=zeros([312 1]);
+              for rev_runner=1:K_training_range_row(k)
+              class_residual(training_matrix_classes(I(1,rev_runner),1),1)=class_residual(training_matrix_classes(I(1,rev_runner),1),1)+1;
+              end
+              [B3, winner_class]=max(class_residual);
+              if(validation_matrix_labels(v,2)==winner_class)
+              score(K_training_range_row(k),1)=score(K_training_range_row(k),1)+1;
+              end
+    end
+    
+end
+[B optimum_K]=max(score(:,1));
+accuracy=B/validation_matrix_size;
